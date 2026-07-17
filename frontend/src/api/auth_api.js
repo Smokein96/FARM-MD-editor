7120363d6d48a51459012c20e732d395c8187746
Anshu <anshu.patil2005@gmail.com>
2026-07-17 22:46:39 +0530

    minor issue fix

--- frontend/src/api/auth_api.js
+++ frontend/src/api/auth_api.js
@@ -1,36 +1,4 @@
-
-base_url = "https://md-x-api.onrender.com"
-
-export async function setup(username, password){
-    const resp = await fetch(
-        `${base_url}/auth/setup`,
-
-        {
-            method : "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-             body : JSON.stringify({
-                username,
-                password
-            })
-        },
-    );
-
-    if (!resp.ok){
-        const error = await resp.json();
-
-        throw {
-            status_code : resp.status,
-            message : error.detail
-        }
-    }
-    
-    return await resp.json();
-}
-
-
-base_url = "https://md-x-api.onrender.com/"
+const base_url = "https://md-x-api.onrender.com"
 
 export async function setup(username, password){
     const resp = await fetch(
