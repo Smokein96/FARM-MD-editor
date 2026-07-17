98d2a6094a4e5feaaec8005111121b99771ff0f2
Anshu <anshu.patil2005@gmail.com>
2026-07-17 18:56:12 +0530

    typo fix

--- frontend/src/pages/logIn.jsx
+++ frontend/src/pages/logIn.jsx
@@ -34,7 +34,7 @@ export function LogIn() {
             notify("Logged in succesfully")
 
         } catch (e) {
-            navigate("error",{
+            navigate("/error",{
                 state:{
                     status_code: e.status_code,
                     error : e.message
